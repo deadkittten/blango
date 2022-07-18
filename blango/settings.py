e387ffbc06a73550fc7ab0dd4f39c38f379404b5
Oleksiy Chugayenko <firestream13@gmail.com>
2022-07-18 15:47:21 +0000

    Finish Introduction and Django Admin, correction 2

--- blango/settings.py
+++ blango/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
 CSRF_COOKIE_SAMESITE = None
 CSRF_TRUSTED_ORIGINS = [os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
