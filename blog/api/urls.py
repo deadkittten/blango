9c3abe0989533dff9a0d44133336756ca75370cd
Oleksiy Chugayenko <firestream13@gmail.com>
2022-09-02 10:53:56 +0000

    Finish related fields

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 
 
-from blog.api.views import PostList, PostDetail
+from blog.api.views import PostList, PostDetail, UserDetail
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
@@ -15,4 +15,5 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
