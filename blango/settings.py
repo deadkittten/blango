7fa97469aad20c94da09e1a7f2227f4cad8d9a48
Oleksiy Chugayenko <firestream13@gmail.com>
2022-09-29 14:40:10 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from datetime import timedelta
 from configurations import Configuration
 from configurations import values
 import dj_database_url
@@ -70,6 +71,7 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -92,6 +94,11 @@ class Dev(Configuration):
             "django_filters.rest_framework.DjangoFilterBackend",
             "rest_framework.filters.OrderingFilter"
         ],
+  }
+
+  SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
   }
 
   SWAGGER_SETTINGS = {
